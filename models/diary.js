b86f1f4042d4f0fdfb760cc908e7402079340af7
AjayaBudha <ajaya.budhathoki@hotmail.com>
2023-05-04 16:32:48 +0100

    all routes added

--- models/diary.js
+++ models/diary.js
@@ -16,6 +16,42 @@ class Diary {
     }
     return response.rows.map((p) => new Diary(p));
   }
+
+  static async getOneById(id) {
+    const response = await db.query("SELECT * from post WHERE post_id = $1", [
+      id,
+    ]);
+    if (response.rows.length != 1) {
+      throw new Error("Unable to locate the post id");
+    }
+    return new Diary(response.rows[0]);
+  }
+
+  static async create(data) {
+    const { title, content } = data;
+    const response = await db.quert(
+      "INSERT INTO post (title, content) VALUES( $1, $2)",
+      [title, content]
+    );
+    const postId = response.rows[0].post_id;
+    const newDiary = await Snack.getOneById([postId]);
+
+    return newDiary;
+  }
+
+  static async update(content, id) {
+    const response = await db.query(
+      "UPDATE post SET content =  $1 WHERE snack_id = $2;",
+      [content, id]
+    );
+  }
+
+  static async destroy(id) {
+    const response = await db.query("DELETE FROM post WHERE post_id = $1 ;", [
+      id,
+    ]);
+    return "The record has been deleted";
+  }
 }
 
 module.exports = Diary;
--- routers/diary.js
+++ routers/diary.js
@@ -5,5 +5,10 @@ const diaryController = require("../controllers/diary");
 const diaryRouter = Router();
 
 diaryRouter.get("/", diaryController.index);
+diaryRouter.get("/", diaryController.create);
+
+diaryRouter.get("/:id", diaryController.show);
+diaryRouter.get("/:id", diaryController.update);
+diaryRouter.get("/:id", diaryController.destroy);
 
 module.exports = diaryRouter;
