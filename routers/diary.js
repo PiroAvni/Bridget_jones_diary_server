82aecaa346252e7e075d7f7acb9d1cf83de7978f
AjayaBudha <ajaya.budhathoki@hotmail.com>
2023-05-04 16:34:51 +0100

    show,create,update, destroy done

--- routers/diary.js
+++ routers/diary.js
@@ -5,10 +5,10 @@ const diaryController = require("../controllers/diary");
 const diaryRouter = Router();
 
 diaryRouter.get("/", diaryController.index);
-diaryRouter.get("/", diaryController.create);
+diaryRouter.post("/", diaryController.create);
 
 diaryRouter.get("/:id", diaryController.show);
-diaryRouter.get("/:id", diaryController.update);
-diaryRouter.get("/:id", diaryController.destroy);
+diaryRouter.patch("/:id", diaryController.update);
+diaryRouter.delete("/:id", diaryController.destroy);
 
 module.exports = diaryRouter;
