7f3c1f68f700582abb5817d07b72428748c9f9df
AjayaBudha <ajaya.budhathoki@hotmail.com>
2023-05-04 14:48:41 +0100

    change file names

--- models/diary.js
+++ models/diary.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const api = require("../app");
+const api = require("./api");
 
 api.listen(process.env.PORT, () => {
   console.log(`API listening on port ${process.env.PORT}...`);
